dfee47236ed4fdef5a1337b81b6e188a6ba9504c
Adam R Moss <adamrmoss@gmail.com>
2025-12-14 01:23:59 -0500

    Start on main UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WindowComponent } from 'ng-luna';
 
 @Component({
     selector: 'app-root',
-    imports: [ RouterOutlet ],
+    imports: [ RouterOutlet, WindowComponent ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent
 {
-    public title = 'edu-basic';
+    public title = 'EduBASIC';
 }
